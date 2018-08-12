f374b665f16384a78a1203a1158b8c0d8779a1ec
Sam Loeschen <samloeschen@gmail.com>
2018-08-11 20:02:56 -0700

    added test bouncing ball boss that uses asset move speed

---
+++ Assets/TestBoss.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBoss : MonoBehaviour {
+
+	public BossParametersAsset parametersAsset;
+	void Update () {
+
+		float speed = parametersAsset.bossParameters.moveSpeed;
+		float t = Mathf.Repeat(Time.time * speed, 1f);
+		float y = Mathf.Abs(Mathf.Sin(t * Mathf.PI * 5f) * (1f - Mathf.Sqrt(t))) * 2f;
+
+		transform.position = Vector3.zero + (Vector3.up * y);
+	}
+}
