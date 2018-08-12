62b6ae1db4367d538261a144c2678bf5f467d211
Sam Loeschen <samloeschen@gmail.com>
2018-08-11 19:18:03 -0700

    challenge part 2

--- Assets/BossCreationMenu.cs
+++ Assets/BossCreationMenu.cs
@@ -6,99 +6,11 @@ using System.IO;
 
 public class BossCreationMenu: EditorWindow {
 
-	BossParametersAsset cloneSourceAsset;
-	BossParametersAsset cloneAsset;
-	SerializedObject cloneAssetSerializedObject;
-	SerializedProperty cloneAssetParametersProperty;
-	bool shouldCopySubfolders = true;
-
-
-	BossParametersAsset newAsset;
-	SerializedObject newAssetSerializedObject; 
-	SerializedProperty newAssetParametersProperty;
-
-	[MenuItem("Asset Creation/Create Boss")]
-	static void Init () {
-		BossCreationMenu menu = (BossCreationMenu)EditorWindow.GetWindow(typeof(BossCreationMenu));
-		menu.Show();
-
-	}
-	void OnGUI () {
-
-		// Always have a new asset in memory that we can edit. This asset doesn't exist on disk yet, it's just our working copy.
-		// Create a serialized object corresponding to the asset, and get the property for boss parameters. 
-		if (newAsset == null || newAssetParametersProperty == null) {
-			newAsset = ScriptableObject.CreateInstance<BossParametersAsset>();
-			newAsset.bossParameters = DefaultBossParameters();
-			newAssetSerializedObject = new SerializedObject(newAsset);
-			newAssetParametersProperty = newAssetSerializedObject.FindProperty("bossParameters");
-		}
-
-		//Draw the new asset editor
-		GUILayout.Space(10);
-		EditorGUILayout.LabelField("Create a new boss asset: ", EditorStyles.largeLabel);
-		GUILayout.Space(5);
-
-		//Draw Property Drawer for the new asset's BossParameters
-		EditorGUILayout.PropertyField(newAssetParametersProperty);
-		newAssetSerializedObject.ApplyModifiedProperties();
-
-		//Draw Create button
-		if (GUILayout.Button("Create")) {
-			CreateNewBoss();
-		}
-
-		//Draw the cloned asset editor
-		GUILayout.Space(20);
-		EditorGUILayout.LabelField("Clone a preexisting boss asset: ", EditorStyles.largeLabel);
-		GUILayout.Space(5);
-		
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("Asset to clone from:");
-
-		BossParametersAsset asset = EditorGUILayout.ObjectField(cloneSourceAsset, typeof(BossParametersAsset), true) as BossParametersAsset;
-		if (asset != cloneSourceAsset){
-			if (asset != null) {
-				cloneAsset = Object.Instantiate(asset);
-				cloneAssetSerializedObject = new SerializedObject(cloneAsset);
-				cloneAssetParametersProperty = cloneAssetSerializedObject.FindProperty("bossParameters");		
-			} else {
-				cloneAssetParametersProperty = null;
-				cloneAsset = null;
-			}
-		}
-		cloneSourceAsset = asset;
-
-		EditorGUILayout.EndHorizontal();
-
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("Clone Boss Subfolders?");
-		shouldCopySubfolders = EditorGUILayout.Toggle(shouldCopySubfolders);
-		EditorGUILayout.EndHorizontal();
-
-		if(cloneAssetParametersProperty != null) {
-			EditorGUILayout.PropertyField(cloneAssetParametersProperty);
-			cloneAssetSerializedObject.ApplyModifiedProperties();
-		}
-
-		if (GUILayout.Button("Clone")) {
-			CloneBoss();
-		}
-	}
-
-	static BossParameters DefaultBossParameters () {
-		BossParameters parameters = new BossParameters();
-		parameters.name = "Psycho Mantis";
-		parameters.subtitle = "NO REFUGE";
-		parameters.strongType = Element.Fire;
-		parameters.weakType = Element.Water;
-		parameters.healthPoints = 100;
-		parameters.attackDelayTime = new LimitedRange(0f, 0f, 10f, 10f);
-		parameters.moveDelayTime = new LimitedRange(0f, 0f, 10f, 10f);
-		parameters.description = "Enter a description here";
-		return parameters;
-	}
-
+	BossParametersAsset cloneSource;
+	BossParametersAsset currentAsset;
+	SerializedObject currentAssetSerializedObject;
+	SerializedProperty currentAssetParametersProperty;
+	
 	private const string rootFolderName = "Bosses";
 	private const string rootFolderPath = "Assets/" + rootFolderName;
 	private static readonly string[] subfolders = new string[] {
@@ -109,76 +21,206 @@ public class BossCreationMenu: EditorWindow {
 		"Prefabs"
 	};
 
-	void CreateNewBoss () {
 
-		var bossName = newAsset.bossParameters.name;
+	[MenuItem("Asset Management/Boss Editor")]
+	static void Init () {
+		BossCreationMenu menu = (BossCreationMenu)EditorWindow.GetWindow(typeof(BossCreationMenu));
+		menu.Show();
+	}
+
+	bool isDeleting = false;
+	void OnGUI () {
+
+		GUILayout.Space(10);
+		if (GUILayout.Button("Create New Boss Asset")) {
+			CreateNewAsset();
+		}
+		GUILayout.Space(10);
+
+		//Edit the asset currently populated in the selected asset object field	
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Selected Boss Asset");
+		BossParametersAsset asset = EditorGUILayout.ObjectField(currentAsset, typeof(BossParametersAsset), true) as BossParametersAsset;
+		EditAsset(asset);
+		EditorGUILayout.EndHorizontal();
+		
+		if (currentAsset != null) {
+
+			EditorGUILayout.PropertyField(currentAssetParametersProperty);
+			currentAssetSerializedObject.ApplyModifiedProperties();
+			currentAsset.name = currentAsset.bossParameters.name;
+			
+			if (!isDeleting) {
+
+				GUILayout.BeginHorizontal();
+
+				// asset clone button. we only present this option if the asset actually exists in the assets folder 
+				if (AssetDatabase.GetAssetPath(currentAsset).Length > 0) {
+					if (GUILayout.Button("Make Clone")) {
+						CloneAsset();
+					}
+
+				//otherwise, we draw the create asset button, which creates new folders for this asset
+				} else {
+					if (GUILayout.Button("Create Asset")) {
+						SaveAsset();
+					}
+				}
+
+				//handle deleting boss. we don't want to delete immediately, so set isDeleting to true
+				if (GUILayout.Button("Delete")) {
+					isDeleting = true;
+				}
+				GUILayout.EndHorizontal();
+
+			//handle isDeleting state
+			} else {
+				
+				GUILayout.BeginHorizontal();
+				GUILayout.FlexibleSpace();
+				GUILayout.Label("Are you sure?");
+				GUILayout.FlexibleSpace();
+				GUILayout.EndHorizontal();
+
+				GUILayout.BeginHorizontal();
+				Color color = GUI.color;
+				GUI.backgroundColor = Color.red;
+				if (GUILayout.Button("Delete")) {
+					DeleteAsset();
+					isDeleting = false;
+				}
+				GUI.backgroundColor = color;
+				if (GUILayout.Button ("Cancel")) {
+					isDeleting = false;
+				}
+				GUILayout.EndHorizontal();
+			}
+		}
+	}
+	void EditAsset (BossParametersAsset asset) {
+		if (asset != null) {
+			if (currentAsset != asset || currentAssetParametersProperty == null) {
+				currentAssetSerializedObject = new SerializedObject(asset);
+				currentAssetParametersProperty = currentAssetSerializedObject.FindProperty("bossParameters");			
+			} 
+		} else {
+			currentAssetSerializedObject = null;
+			currentAssetParametersProperty = null;
+		}
+		currentAsset = asset;
+	}
+	void CreateNewAsset () {
+		BossParametersAsset asset = ScriptableObject.CreateInstance<BossParametersAsset>();
+		asset.bossParameters = DefaultBossParameters();
+		asset.name = asset.bossParameters.name;
+		EditAsset(asset);
+	}
+
+	void CloneAsset () {
+		cloneSource = currentAsset;
+		
+		BossParametersAsset clone = Object.Instantiate(currentAsset);
+		clone.name = currentAsset.name + " Clone";
+		clone.bossParameters.name += " Clone";
+		EditAsset(clone);
+	}
+	
+	void DeleteAsset () {
+		string parentDirectory = Path.GetDirectoryName(AssetDatabase.GetAssetPath(currentAsset));
+		currentAsset = null;
+		FileUtil.DeleteFileOrDirectory(parentDirectory);
+		Debug.Log(parentDirectory);
+		AssetDatabase.Refresh();
+	}
+
+	void SaveAsset() {
+
+		//create boss root path if it doesn't exist
 		if (!AssetDatabase.IsValidFolder(rootFolderPath)) {
 			AssetDatabase.CreateFolder("Assets", rootFolderName);
 		}
 
-		//create boss path
-		string bossPath = MakeBossDirectory(bossName);
-		if (bossPath == null) { return; }
-
-		// create our scriptable object
-		SaveBossParametersAsset(newAsset, bossPath, bossName);
-
-		//create subfolders
-		for (int i = 0; i < subfolders.Length; i++) {
-			AssetDatabase.CreateFolder(bossPath, subfolders[i]);
+		//create the boss folder. if it exists already, return with an error notification
+		string bossName = currentAsset.bossParameters.name;
+		string bossPath = rootFolderPath + "/" + bossName;
+		if (AssetDatabase.IsValidFolder(bossPath)) {
+			ShowNotification(new GUIContent("Error: Assets for boss " + bossName + " already exist!"));
+			return;
 		}
+		AssetDatabase.CreateFolder(rootFolderPath, bossName);
+
+		//create the boss asset 
+		string assetPath = bossPath + "/" + bossName + ".asset";
+		AssetDatabase.CreateAsset(currentAsset, assetPath);
+		AssetDatabase.SaveAssets();
+
+		//create subfolders. if we have any cloned subfolder paths we are supposed to copy,
+		//copy them instead of creating new subfolders
+		if(cloneSource != null) {
+			CloneSubfolders(cloneSource, bossPath);
+		} else {
+			CreateNewSubfolders(bossPath);
+		}
+		AssetDatabase.Refresh();
 	}
 
-	void CloneBoss () {
-		
-		string bossName = cloneAsset.bossParameters.name;
-		string bossPath = MakeBossDirectory(bossName);
-		if (bossPath == null) { return; }
+	void CloneSubfolders (BossParametersAsset source, string path) {
+		string parentDirectory = Path.GetDirectoryName(AssetDatabase.GetAssetPath(cloneSource));
+		string[] parentDirectoryContents = AssetDatabase.GetSubFolders(parentDirectory);
 
-		SaveBossParametersAsset(cloneAsset, bossPath, bossName);
-
-		if (shouldCopySubfolders) {
-
-			//get parent directory of bossToCopy
-			string parentDirectory = Path.GetDirectoryName(AssetDatabase.GetAssetPath(cloneAsset));
-			string[] parentDirectoryContents = AssetDatabase.GetSubFolders(parentDirectory);
-
-			// iterate through the subfolders array and copy any matching folders to the new boss asset folder
-			for (int i = 0; i < subfolders.Length; i++) {
-				bool foundMatch = false;
-				for (int j = 0; j < parentDirectoryContents.Length; j++) {
-					string itemName = new DirectoryInfo(parentDirectoryContents[j]).Name;
-					if (itemName == subfolders[i]) {
-						FileUtil.CopyFileOrDirectory(parentDirectoryContents[j], bossPath + "/" + subfolders[i]);
-						foundMatch = true;
-					}
+		// iterate through the subfolders array and copy any matching folders to the new boss asset folder
+		for (int i = 0; i < subfolders.Length; i++) {
+			bool clonedSubfolder = false;
+			for (int j = 0; j < parentDirectoryContents.Length; j++) {
+				string itemName = new DirectoryInfo(parentDirectoryContents[j]).Name;
+				if (itemName == subfolders[i]) {
+					FileUtil.CopyFileOrDirectory(parentDirectoryContents[j], path + "/" + subfolders[i]);
+					clonedSubfolder = true;
 				}
-
-				//make sure to fill any folders that were missing!
-				if (!foundMatch) {
-					AssetDatabase.CreateFolder(bossPath, subfolders[i]);
-				}
-				AssetDatabase.Refresh();
+			}
+			//make sure to fill any folders that were missing!
+			if (!clonedSubfolder) {
+				AssetDatabase.CreateFolder(path, subfolders[i]);
 			}
 		}
 	}
 
-	string MakeBossDirectory (string bossName) {
-		// make sure a boss directory for this name doesn't already exist
-		string bossPath = rootFolderPath + "/" + bossName;
-		if (AssetDatabase.IsValidFolder(bossPath)) {
-			ShowNotification(new GUIContent("Error: Assets for boss " + bossName + " already exist!"));
-			return null;
+	void CreateNewSubfolders (string path) {
+		for (int i = 0; i < subfolders.Length; i++) {
+			AssetDatabase.CreateFolder(path, subfolders[i]);	
 		}
-		AssetDatabase.CreateFolder(rootFolderPath, bossName);
-		return bossPath;
 	}
 
-	private void SaveBossParametersAsset (BossParametersAsset asset, string path, string bossName) {
-		string assetPath = path + "/" + bossName + ".asset";
-		AssetDatabase.CreateAsset(asset, assetPath);
-		AssetDatabase.SaveAssets();
+	static BossParameters DefaultBossParameters () {
+		BossParameters parameters = new BossParameters();
+		parameters.name = GetDefaultBossName();
+		parameters.subtitle = "NO REFUGE";
+		parameters.strongType = Element.Fire;
+		parameters.weakType = Element.Water;
+		parameters.healthPoints = 100;
+		parameters.attackDelayTime = new LimitedRange(0f, 0f, 10f, 10f);
+		parameters.moveDelayTime = new LimitedRange(0f, 0f, 10f, 10f);
+		parameters.description = "Enter a description here";
+		return parameters;
 	}
 
+	static readonly string[] bossNames = new string[] {
+		"Psycho Mantis",
+		"GLaDOS",
+		"Doctor Robotnik",
+		"Bowser",
+		"Ridley",
+		"Mike Tyson",
+		"Lavos",
+		"Big Daddy",
+		"Master Hand",
+		"Ganon",
+		"Giygas",
+		"Eve"
+	};
+	static string GetDefaultBossName () {
+		int idx = Random.Range(0, bossNames.Length);
+		return bossNames[idx];
+	}
 }
 
